2d03471afde810c2eee6d215408190dfd5578085
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2024-01-13 17:21:23 +0600

    Update tabulation.cpp

--- Leetcode/518. Coin Change II/tabulation.cpp
+++ Leetcode/518. Coin Change II/tabulation.cpp
@@ -1,43 +1,14 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int n=coins.size(),i,j;
-        vector<vector<int>>v(n,vector<int>(amount+1));
-        for(i=0;i<n;i++)
-        {
-            v[i][0]=1;
-        }
-        if(coins[0]<=amount)
-        {
-            for(i=coins[0];i<=amount;i++)
-            {
-                if(i%coins[0]==0)
-                {
-                    v[0][i]=1;
-                }
+        vector<int> dp(amount+1);
+        dp[0] = 1;
+        for(auto& x:coins){
+            for(int i=x;i<=amount;i++){
+                //cout<<dp[i]<<" "<<dp[i-1]<<endl;
+                dp[i] += dp[i-x];
             }
         }
-        for(i=1;i<n;i++)
-        {
-            if(coins[i]>amount)
-            {
-                v[i]=v[i-1];
-                continue;
-            }
-            for(j=0;j<coins[i];j++)
-            {
-                v[i][j]=v[i-1][j];
-            }
-            for(j=coins[i];j<=amount;j++)
-            {
-                v[i][j]=v[i][j]+v[i][j-coins[i]];
-            }
-        for(auto g:v[i])
-        {
-            cout<<g<<" ";
-        }
-        cout<<endl;
-        }
-        return v[n-1][amount];
+        return dp[amount];
     }
 };
